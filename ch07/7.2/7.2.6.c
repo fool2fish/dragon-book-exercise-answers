52df89305eef6f0c5fe81e02040a77f2c65c50b0
fool2fish <fool2fish@gmail.com>
2013-10-04 09:32:20 +0800

    Update unfinished 7.2.6 answser

---
+++ ch07/7.2/7.2.6.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int f(int x, int *py, int **ppz) {
+	**ppz += 1;
+	*py += 2;
+	x += 3;
+	return x + *py + **ppz;
+
+}
+
+int main() {
+	int c = 4;
+	int *b = &c;
+	int *a = &b;
+    printf(f(c, *b, **a));
+
+}
